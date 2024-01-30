13b0b1162a79aee185d685c3671f485481427330
Sara <sala0002@stud.kea.dk>
2024-01-30 13:54:29 +0100

    rettelse

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ const all = document.querySelectorAll(".container div");
 
 setInterval(() => {
   all.forEach((bubble) => {
-    bubble.style.transform = `translate(${Math.random() * 100}vw, ${
-      Math.random() * 100
-    }vh)`;
-    bubble.style.background = `hsl(${Math.random() * 360}, 70%, 70%)`;
+    bubble.style.transform = `translate(${Math.random() * 100}vw, ${Math.random() * 100}vh)`;
+    bubble.style.background = `hsl(${Math.random() * 360}, 90%, 90%)`;
   });
-}, 2000);
+}, 1000);

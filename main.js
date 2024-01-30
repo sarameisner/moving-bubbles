c91d507abb43176df299abbba43bd8f87a0dc938
Sara <sala0002@stud.kea.dk>
2024-01-30 14:29:55 +0100

    ret

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const all = document.querySelectorAll(".container div");
 setInterval(() => {
   all.forEach((bubble) => {
     bubble.style.transform = `translate(${Math.random() * 100}vw, ${Math.random() * 100}vh)`;
-    bubble.style.background = `hsl(${Math.random() * 360}, 90%, 90%)`;
+    bubble.style.background = `hsl(${Math.random() * 360}, 80%, 80%)`;
   });
 }, 1000);
